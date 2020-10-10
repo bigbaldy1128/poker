bd919a7d66d627ddec4a31a75c9bcc3eaaa6230b
bigbaldy1128 <bigbaldy@163.com>
2020-10-10 15:25:58 +0800

    add docker file

--- src/main/java/com/bigbaldy/poker/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/bigbaldy/poker/exception/handler/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import com.bigbaldy.poker.exception.AbstractUncheckedException;
 import com.bigbaldy.poker.exception.BaseErrorInfo;
 import com.bigbaldy.poker.resource.ResponseResource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
@@ -13,26 +15,29 @@ import org.springframework.web.client.HttpClientErrorException;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler
-    public ResponseResource<?> runtimeExceptionHandler(Exception e) {
+    public ResponseEntity<ResponseResource<?>> runtimeExceptionHandler(Exception e) {
         log.error(e.getMessage(), e);
-        return ResponseResource.builder()
+        ResponseResource<?> responseResource = ResponseResource.builder()
                 .code(BaseErrorInfo.FAILURE.getCode())
                 .build();
+        return new ResponseEntity<>(responseResource, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(HttpClientErrorException.class)
-    public ResponseResource<?> businessExceptionHandler(HttpClientErrorException e) {
-        return ResponseResource.builder()
+    public ResponseEntity<ResponseResource<?>> businessExceptionHandler(HttpClientErrorException e) {
+        ResponseResource<?> responseResource = ResponseResource.builder()
                 .code(BaseErrorInfo.HTTP_CLIENT_ERROR.getCode())
                 .message(e.getMessage())
                 .build();
+        return new ResponseEntity<>(responseResource, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(AbstractUncheckedException.class)
-    public ResponseResource<?> businessExceptionHandler(AbstractUncheckedException e) {
-        return ResponseResource.builder()
+    public ResponseEntity<ResponseResource<?>> businessExceptionHandler(AbstractUncheckedException e) {
+        ResponseResource<?> responseResource = ResponseResource.builder()
                 .code(e.getCode())
                 .message(e.getMessage())
                 .build();
+        return new ResponseEntity<>(responseResource, HttpStatus.OK);
     }
 }
